7f981a5f7f3cc9550187243e5e5fd16c39943df9
Mirat YILDIRIM <77025193+muratyildirim89@users.noreply.github.com>
2022-10-31 15:00:22 +0300

    Delete generics directory

--- generics/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        MyList<Customer> sehirler=new MyList<Customer>();
-        sehirler.add(new Customer());
-        sehirler.add(new Customer());
-    }
-}
